08cb31d3752a4e4c447ca9962166967a462775df
Hari <hari.stpeters@gmail.com>
2019-05-14 11:34:33 +0200

    fixed pom and bootstrap class

--- src/main/java/nl/hari/TraceExampleApplication.java
+++ src/main/java/nl/hari/TraceExampleApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-//@CamelOpenTracing
+@CamelOpenTracing
 public class TraceExampleApplication {
 //	@EndpointInject(uri = "direct:testMsg")
 	private FluentProducerTemplate producer;
